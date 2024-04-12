662cc15a19324f1a3829590ef2cc11839362ca40
ykamoji <ykamoji@umass.edu>
2024-04-12 13:17:01 -0400

    Added images for visualization

--- visualisation.py
+++ visualisation.py
@@ -18,6 +18,8 @@ warnings.filterwarnings('ignore')
 
 def visualize(Args):
 
+    outputPath = Args.Visualization.Output
+
     images = glob.glob("images/*.JPEG")
     Device = Args.Visualization.Model.Device
 
@@ -30,18 +32,24 @@ def visualize(Args):
                                                   cache_dir=Args.Visualization.Model.CachePath)
 
     for im in images:
+
+        label = im.split('_')[1].split('.')[0]
         image = Image.open(im)
-        factor = 8
+        factor = 14
 
         img_resized = resize(np.array(image), size=(224, 224), resample=PILImageResampling.BILINEAR)
         grid_color = [0, 0, 0]
         grid_size = 224 // factor
-        # img_resized_grid = img_resized.copy()
-        # img_resized_grid[:, ::grid_size, :] = grid_color
-        # img_resized_grid[::grid_size, :, :] = grid_color
-        #
-        # plt.imshow(img_resized_grid)
+        img_resized_grid = img_resized.copy()
+        img_resized_grid[:, ::grid_size, :] = grid_color
+        img_resized_grid[::grid_size, :, :] = grid_color
+
+        fig = plt.figure()
+        plt.imshow(img_resized_grid)
+        plt.axis('off')
+        plt.savefig(outputPath+f"{label}_1_grid")
         # plt.show()
+        plt.close(fig)
 
         inputs = processor(images=image, return_tensors="pt")
         inputs.to(Device)
@@ -49,7 +57,7 @@ def visualize(Args):
                               output_globenc=True)
         logits = outputs.logits
         predicted_class_idx = logits.argmax(-1).item()
-        print(f"Actual: {im.split('_')[1].split('.')[0].ljust(10, ' ')} Predicted: {model.config.id2label[predicted_class_idx]}")
+        print(f"Actual: {label.ljust(10, ' ')} Predicted: {model.config.id2label[predicted_class_idx]}")
 
         num_layers = len(outputs.attentions)
         norm_nenc = torch.stack([outputs.attributions[i][4] for i in range(num_layers)]).detach().squeeze().cpu().numpy()
@@ -71,6 +79,8 @@ def visualize(Args):
         # print(others.shape)
         all_patches = []
         step = 196 // (factor**2)
+        if step == 0:
+            step = 1
         for layer in range(num_layers):
             patches = []
             for patch in range(0, 196, step):
@@ -80,20 +90,25 @@ def visualize(Args):
             all_patches.append(patches)
 
         all_patches = np.array(all_patches)
-        all_patches = all_patches[:,:-1]
+        # print(all_patches.shape)
+        if step != 1:
+            all_patches = all_patches[:,:-1]
         # print(cls)
 
-        # plt.figure(figsize=(7, 7))
-        # df = pd.DataFrame(all_patches, columns=np.arange(1, all_patches.shape[1]+1), index=range(len(all_patches), 0, -1))
-        # ax = sns.heatmap(df, cmap="Reds", square=True)
-        # bottom, top = ax.get_ylim()
-        # ax.set_ylim(bottom + 0.5, top - 0.5)
-        # plt.title("GlobEnc", fontsize=16)
+        plt.figure(figsize=(7, 7))
+        df = pd.DataFrame(all_patches, columns=np.arange(1, all_patches.shape[1]+1), index=range(len(all_patches), 0, -1))
+        ax = sns.heatmap(df, cmap="Reds", square=True)
+        bottom, top = ax.get_ylim()
+        ax.set_ylim(bottom + 0.5, top - 0.5)
+        plt.title("GlobEnc", fontsize=16)
         # plt.ylabel("Layer", fontsize=16)
         # plt.xticks(rotation = 90, fontsize=16)
-        # plt.yticks(fontsize=13)
-        # plt.gcf().subplots_adjust(bottom=0.2)
-        #
+        plt.xticks([])
+        # plt.yticks(fontsize=9)
+        plt.yticks([])
+        plt.gcf().subplots_adjust(bottom=0.2)
+        plt.savefig(outputPath + f"{label}_3_heatmap")
+        # plt.close(fig)
         # plt.show()
 
         # print(all_patches[0])
@@ -102,11 +117,13 @@ def visualize(Args):
 
         attribute_score_per_patch = [1 if all_patches[0, i] > mean else 0 for i in range(all_patches.shape[1])]
 
-        # print(attribute_score_per_patch)
+        # print(len(attribute_score_per_patch))
 
         img_resized_final = img_resized.copy()
         fig, ax = plt.subplots()
         ax.imshow(img_resized_final)
+        if factor > 14:
+            factor = 14
         x, y = np.meshgrid(np.arange(0, factor), np.arange(0, factor), indexing='ij')
 
         for i in range(factor):
@@ -118,12 +135,7 @@ def visualize(Args):
 
                     ax.add_patch(rect)
 
-        plt.show()
-
-
-
-
-
-
-
-
+        plt.imshow(img_resized_final)
+        plt.axis('off')
+        plt.savefig(outputPath + f"{label}_2_attributes")
+        # plt.show()
